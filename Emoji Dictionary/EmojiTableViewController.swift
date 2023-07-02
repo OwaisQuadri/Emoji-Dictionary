283a8f2e4dee64cfeed1f27be801233f826420b8
Owais Quadri <owaisquadri01@gmail.com>
2023-07-02 06:01:05 -0400

    Create table view controller for emojis

---
+++ Emoji Dictionary/EmojiTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  EmojiTableViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Owais Quadri on 2023-07-02.
+//
+
+import UIKit
+
+class EmojiTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+    
+    // number of rows
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 5
+    }
+
+    // What goes in each row
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "emojiCell", for: indexPath)
+
+         // Configure the cell...
+        cell.textLabel?.text = "Hello"
+
+        return cell
+    }
+
+}
